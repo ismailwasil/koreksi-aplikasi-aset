9d8b987938fe856c1d3df3eb47d4f6b812520284
Ismail ibn Wasil <75897987+ismailwasil@users.noreply.github.com>
2026-04-09 22:45:35 +0700

    Create tombolKaburNormal.js

---
+++ tombolKaburNormal.js
@@ -0,0 +1,13 @@
+document.addEventListener("DOMContentLoaded", () => {
+    let ajukanBtn;
+    if(idMasuk == 5){
+        ajukanBtn=document.getElementById('pengajuanPUPR');
+        // onclick="bukaPWD()"
+        ajukanBtn.addEventListener('click', bukaPWD);
+    }else{
+        ajukanBtn=document.getElementById('btnAjuAll');
+        // mengatur kondisi bisa mengajukan saat pertama dimuat
+        ajukanBtn.setAttribute('data-bs-toggle','modal');
+        ajukanBtn.setAttribute('data-bs-target','#ajukanSPM');
+    }
+});
