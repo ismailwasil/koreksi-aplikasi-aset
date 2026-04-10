7ec9e9b890f486fd81f9c18c2506a0a26e8b973f
Ismail ibn Wasil <75897987+ismailwasil@users.noreply.github.com>
2026-04-11 00:27:07 +0700

    Update JamKerja_model.php

--- JamKerja_model.php
+++ JamKerja_model.php
@@ -13,4 +13,24 @@ class JamKerja_model extends CI_Model
             ->get('jam_kerja')
             ->row();
     }
+
+    public function getDataJamKerja()
+    {
+        return $this->db->get('jam_kerja')->result_array();
+    }
+
+    public function getHariLibur()
+    {
+        return $this->db->get('hari_libnas')->result_array();
+    }
+
+    public function updateJamKerja($id, $jam_mulai, $jam_selesai, $status)
+    {
+        $this->db->where('id_jk', $id);
+        return $this->db->update('jam_kerja', [
+            'jam_mulai'   => $jam_mulai,
+            'jam_selesai'    => $jam_selesai,
+            'aktif_jk' => $status
+        ]);
+    }
 }
